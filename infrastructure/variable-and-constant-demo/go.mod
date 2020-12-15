908ec4b6eaf461c3b56dbaba57efa7e5873ad8ea
Not-the-expected <18715084388@163.com>
2020-12-15 22:08:12 +0800

    basic variable

---
+++ infrastructure/variable-and-constant-demo/go.mod
@@ -0,0 +1,3 @@
+module VCdemo
+
+go 1.15
---
+++ infrastructure/variable-and-constant-demo/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+// 声明变量
+func declareVariable()  {
+	var s string
+	var (
+		age int8
+		name string = "令狐冲"
+	)
+	fmt.Println(s,age,name)
+	name , address := "蓝盈盈","华山"
+	fmt.Println(name,address)
+}
+
+func getTheFlag() *string {
+	return flag.String("name","everyone","the greeting object")
+}
+
+var block = "package"
+
+
+func main()  {
+	//declareVariable()
+
+	//var name = getTheFlag()
+	//flag.Parse()
+	//fmt.Printf("Hello, %v!\n", *name)
+
+	block := "function"
+	{
+		block := "inner"
+		fmt.Printf("The block is %s.\n", block)
+	}
+	fmt.Printf("The block is %s.\n", block)
+
+}
